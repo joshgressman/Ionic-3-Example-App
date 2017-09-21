a6d6011e80f202fe591438e1d9b7b5ae430010df
joshgressman <joshgressman@gmail.com>
2017-09-21 16:01:01 -0500

    added page navigation

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { ContactPage } from '../contact/contact';
 
-/**
- * Generated class for the AboutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html',
@@ -18,8 +13,20 @@ export class AboutPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AboutPage');
-  }
+  goTo(page) {
+         if (page === 'home') {
+            this.navCtrl.push(HomePage);
+         } else if (page === 'contact') {
+            this.navCtrl.push(ContactPage);
+         }
+
+
+
+ }
+      back() {
+         if (this.navCtrl.length() >= 2) {
+            this.navCtrl.pop();
+         }
+}
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { AboutPage } from '../about/about';
 
-/**
- * Generated class for the ContactPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html',
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactPage');
-  }
+   goTo(page) {
+         if (page === 'home') {
+            this.navCtrl.push(HomePage);
+         } else if (page === 'about') {
+            this.navCtrl.push(AboutPage);
+         }
+}
+      back() {
+         if (this.navCtrl.length() >= 2) {
+            this.navCtrl.pop();
+         }
+}
 
 }
